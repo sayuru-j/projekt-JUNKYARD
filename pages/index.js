c1d51eac10c403c9f3d9a603d74fa502c219d4d3
Sayuru <zayuru.ai@gmail.com>
2023-03-10 19:39:29 +0530

    Added HomeBanner

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import Layout from '../components/Layout'
 
 const HomePage = () => {
+
+  const homeBanner = () => (
+    <div className='welcome-container'>
+      <div className='welcome-text'>
+      <h2>Zero Waste</h2>
+      <h5>Sustainable living starts with Zero Waste. Let's keep Sri Lanka clean and green.</h5>
+      </div>
+    </div>
+  )
+  
     return <Layout><div className='container-home'>
-      <img src='/static/images/home-page-banner.jpg' alt='home-page-banner-truck' width="100%"></img>
+      {homeBanner()}
       </div></Layout>
   }
   
