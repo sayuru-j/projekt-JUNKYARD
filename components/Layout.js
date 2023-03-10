917da5f66a3baa0106476d4409ac689b0991d551
Sayuru <zayuru.ai@gmail.com>
2023-03-10 14:23:46 +0530

    added logo

--- components/Layout.js
+++ components/Layout.js
@@ -6,6 +6,7 @@ import nProgress from "nprogress";
 import "nprogress/nprogress.css"
 
 
+
 Router.onRouteChangeStart = url => nProgress.start()
 Router.onRouteChangeComplete = url => nProgress.done()
 Router.onRouteChangeError = url => nProgress.done();
@@ -18,17 +19,21 @@ const Layout = ({children}) => {
             rel="stylesheet" 
             href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" 
             integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" 
-            crossorigin="anonymous"/>
+            crossOrigin="anonymous"/>
             <link rel="stylesheet" href="/static/css/styles.css"/>
            </header>
            </React.Fragment>
+    }
+
+    const brand = () => {
+        return  <img src="/static/images/junkyard-logo.png" alt="jy-logo" width="100" height="100"/> 
     }
             
     
             
 
     const nav = () => (
-                    <ul class="nav nav-tabs bg-dark">
+                    <ul className="nav nav-tabs bg-dark">
             <li class="nav-item">
                 <Link className="nav-link text-light" href="/">Home</Link>
             </li>
@@ -53,12 +58,12 @@ const Layout = ({children}) => {
     const footer = () => (
         <div class="container">
             <footer class="py-3 my-4">
-                <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+                <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+                <li className="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+                <li className="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
+                <li className="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
+                <li className="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+                <li className="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
                 </ul>
                 <p class="text-center text-muted">&copy; 2021 Company, Inc</p>
             </footer>
@@ -67,7 +72,7 @@ const Layout = ({children}) => {
 
     return <React.Fragment>
         {head()} 
-        
+        {brand()}
         {nav()} 
         
         {children} 
