c23f646425bf2c6c705252d1da4bd3d5ca9ea064
Sayuru <zayuru.ai@gmail.com>
2023-03-10 19:14:45 +0530

    Added home page banner

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Layout from '../components/Layout'
 
 const HomePage = () => {
-    return <Layout><div className='container-home'>Home</div></Layout>
+    return <Layout><div className='container-home'>
+      <img src='/static/images/home-page-banner.jpg' alt='home-page-banner-truck' width="100%"></img>
+      </div></Layout>
   }
   
   export default HomePage;
