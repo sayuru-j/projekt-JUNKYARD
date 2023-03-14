a8181dc3fb8f894e46ebb0d7a78437eb7bbee4fc
Sayuru <zayuru.ai@gmail.com>
2023-03-14 16:43:46 +0530

    Changed the navbar

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -29,33 +29,40 @@ const Layout = ({children}) => {
             
 
     const nav = () => (
-                    <ul className="nav nav-custom">
-                            <a href="/"><img className="brand-logo" src="/static/images/junkyard-logo.png" alt="jy-logo"/></a>
-                        
-            <li className="nav-item">
-                <Link className="nav-link text-dark" href="/">Home</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link text-dark" href="/collect">Collect</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link text-dark" href="/center">Recycling Centers</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link text-dark" href="/product">Products</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link text-dark" href="/fleet">Fleet</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link text-dark" href="/aboutus">About Us</Link>
-            </li>
-            <div className="sign-up-btn">
-            <Link href="/signup">
-            <Button variant="contained" color="success">Sign Up For Services</Button>
-            </Link>
-            </div>
-            </ul>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light nav-custom">
+  <a href="/"><img className="brand-logo" src="/static/images/junkyard-logo.png" alt="jy-logo"/></a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item">
+        <Link className="nav-link" href="/">Home <span className="sr-only">(current)</span></Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" href="/collect">Collect <span className="sr-only">(current)</span></Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" href="/center">Our Centers <span className="sr-only">(current)</span></Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" href="/product">Recycled Products <span className="sr-only">(current)</span></Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" href="/fleet">Our Fleet <span className="sr-only">(current)</span></Link>
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" href="/aboutus">About Us <span className="sr-only">(current)</span></Link>
+      </li>
+    </ul>
+    <div className="sign-up-btn my-2 my-lg-0">
+    <Link href="/signup">
+    <Button variant="contained" color="success">Sign Up For Services</Button>
+    </Link>
+    </div>
+  </div>
+</nav>
             
             
     )
@@ -64,12 +71,12 @@ const Layout = ({children}) => {
             <footer className="py-3 container-footer">
                 <ul className="nav justify-content-center border-bottom pb-3 mb-3">
                 <li className="nav-item"><Link href="/" as="/" className="nav-link px-2 text-muted">Home</Link></li>
-                <li className="nav-item"><Link href="#" as="#" className="nav-link px-2 text-muted">Features</Link></li>
-                <li className="nav-item"><Link href="#" as="#" className="nav-link px-2 text-muted">Pricing</Link></li>
-                <li className="nav-item"><Link href="#" as="#" className="nav-link px-2 text-muted">FAQs</Link></li>
-                <li className="nav-item"><Link href="#" as="#" className="nav-link px-2 text-muted">About</Link></li>
+                <li className="nav-item"><Link href="#" as="#" className="nav-link px-2 text-muted">Collect</Link></li>
+                <li className="nav-item"><Link href="#" as="#" className="nav-link px-2 text-muted">Visit a Center</Link></li>
+                <li className="nav-item"><Link href="#" as="#" className="nav-link px-2 text-muted">Buy Recycled Products</Link></li>
+                <li className="nav-item"><Link href="#" as="#" className="nav-link px-2 text-muted">Garbage Trucks</Link></li>
                 </ul>
-                <p className="text-center text-muted">&copy; 2021 Company, Inc</p>
+                <p className="text-center text-muted">JUNKYARD © 2023 SJ, Inc.</p>
             </footer>
     )
 
