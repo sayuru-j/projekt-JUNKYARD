5a747426a15f5333db588dd9f207ad59dbf7d9b7
Sayuru <zayuru.ai@gmail.com>
2023-03-14 18:55:50 +0530

    .

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -24,6 +24,7 @@ const Layout = ({children}) => {
             <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900italic,900' rel='stylesheet' type='text/css'></link>
             <link href="https://fonts.googleapis.com/css?family=Montserrat:100" rel="stylesheet"></link>
             <link rel="shortcut icon" href="#"></link>
+            
            </header>
            </React.Fragment>
     ) 
